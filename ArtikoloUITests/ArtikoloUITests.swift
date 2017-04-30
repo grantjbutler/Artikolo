0346b53dcee3eba518bfc699fb531c9943ced144
Grant Butler <grantjbutler@users.noreply.github.com>
2017-04-30 19:33:53 -0400

    reset ui tests.

--- ArtikoloUITests/ArtikoloUITests.swift
+++ ArtikoloUITests/ArtikoloUITests.swift
@@ -31,8 +31,6 @@ class ArtikoloUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        let articleListTable = XCUIApplication().tables["Article List"]
-        
     }
     
 }
