f2961e21104b1c38cfbde8b458921a5cf4c9deb8
Grant Butler <grantjbutler@users.noreply.github.com>
2017-05-01 21:35:54 -0400

    Replace Interstellar with RxSwift.

--- Carthage/Checkouts/Interstellar/Tests/LinuxMain.swift
+++
@@ -1,6 +0,0 @@
-import XCTest
-@testable import InterstellarTests
-
-XCTMain([
-  testCase(ObservableTests.allTests)
-])
--- Carthage/Checkouts/Interstellar/Tests/linux.sh
+++
@@ -1,1 +0,0 @@
-docker run --rm -i -t -v $(pwd):/root/interstellar -w /root/interstellar ibmcom/swift-ubuntu:latest swift test
