7795691569ee088623dbeec9a8fc8d0b25529d3f
Grant Butler <grantjbutler@users.noreply.github.com>
2017-05-13 18:26:30 -0400

    Add ArticleBrowserCoordinator.

---
+++ Artikolo/ArticleBrowserCoordinator.swift
@@ -0,0 +1,27 @@
+//
+//  ArticleBrowserCoordinator.swift
+//  Artikolo
+//
+//  Created by Grant Butler on 5/13/17.
+//  Copyright © 2017 Grant Butler. All rights reserved.
+//
+
+import UIKit
+import Dip
+
+class ArticleBrowserCoordinator: Coordinator {
+    
+    let article: Article
+    
+    init(navigationController: UINavigationController, container: DependencyContainer, article: Article) {
+        self.article = article
+        
+        super.init(navigationController: navigationController, container: container)
+    }
+    
+    override func start() {
+        let viewController = ArticleBrowserViewController(article: article)
+        navigationController.pushViewController(viewController, animated: true)
+    }
+    
+}
