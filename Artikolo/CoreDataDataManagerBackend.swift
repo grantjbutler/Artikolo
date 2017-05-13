86ec1408f0c5434860ab4749e9827af682476202
Grant Butler <grantjbutler@users.noreply.github.com>
2017-05-13 19:09:19 -0400

    Add method to delete incompatible stores.

--- Artikolo/CoreDataDataManagerBackend.swift
+++ Artikolo/CoreDataDataManagerBackend.swift
@@ -11,6 +11,18 @@ import CoreData
 import RxSwift
 import RxCoreData
 
+private extension NSPersistentContainer {
+    
+    func removeStores() throws {
+        let coordinator = persistentStoreCoordinator
+        try persistentStoreDescriptions.forEach({ (store) in
+            guard let storeURL = store.url else { return }
+            try coordinator.destroyPersistentStore(at: storeURL, ofType: store.type, options: store.options)
+        })
+    }
+    
+}
+
 class CoreDataDataManagerBackend: DataManagerBackend {
     
     private let container: NSPersistentContainer
@@ -25,21 +37,18 @@ class CoreDataDataManagerBackend: DataManagerBackend {
          error conditions that could cause the creation of the store to fail.
         */
         let container = NSPersistentContainer(name: "Artikolo")
+        container.persistentStoreDescriptions.forEach { $0.shouldMigrateStoreAutomatically = true }
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
+            guard let error = error else { return }
+            print("Got error, reseting container to attempt a resolve: \(error)")
+            
+            try! container.removeStores()
+            
+            container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+                if let error = error as NSError? {
+                    fatalError("Unresolved error \(error), \(error.userInfo)")
+                }
+            })
         })
         return container
     }
