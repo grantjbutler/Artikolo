73ad47224f873aa63abea20f7d6f43323253ad92
Grant Butler <grantjbutler@users.noreply.github.com>
2017-05-13 16:08:02 -0400

    Add reactive Core Data framework.

---
+++ Carthage/Checkouts/RxCoreData/Example/RxCoreData/ViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ViewController.swift
+//  RxCoreData
+//
+//  Created by Scott Gardner on 05/18/2016.
+//  Copyright (c) 2016 Scott Gardner. All rights reserved.
+//
+
+import UIKit
+import CoreData
+import RxSwift
+import RxCocoa
+import RxDataSources
+import RxCoreData
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var addBarButtonItem: UIBarButtonItem!
+    
+    var managedObjectContext: NSManagedObjectContext!
+    let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        bindUI()
+        configureTableView()
+    }
+    
+    func bindUI() {
+        addBarButtonItem.rx.tap
+            .map { _ in
+                Event(id: UUID().uuidString, date: Date())
+            }.subscribe(onNext: { [weak self] (event) in
+                _ = try? self?.managedObjectContext.rx.update(event)
+            })
+            .addDisposableTo(disposeBag)
+    }
+    
+    func configureTableView() {
+        tableView.isEditing = true
+        
+        /*
+         // Non-animated
+        
+         managedObjectContext.rx.entities(Event.self,
+                                          sortDescriptors: [NSSortDescriptor(key: "date", ascending: false)])
+            .bindTo(tableView.rx.items(cellIdentifier: "Cell")) { row, event, cell in
+                cell.textLabel?.text = "\(event.date)"
+            }
+            .addDisposableTo(disposeBag)
+        */
+        
+        // Animated
+
+        let animatedDataSource = RxTableViewSectionedAnimatedDataSource<AnimatableSectionModel<String, Event>>()
+        animatedDataSource.configureCell = { dateSource, tableView, indexPath, event in
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+            cell.textLabel?.text = "\(event.date)"
+            return cell
+        }
+        
+        managedObjectContext.rx.entities(Event.self, sortDescriptors: [NSSortDescriptor(key: "date", ascending: false)])
+            .map { events in
+                [AnimatableSectionModel(model: "Section 1", items: events)]
+            }
+            .bindTo(tableView.rx.items(dataSource: animatedDataSource))
+            .addDisposableTo(disposeBag)
+ 
+        self.tableView.rx.itemDeleted.map { [unowned self] ip -> Event in
+            return try self.tableView.rx.model(at: ip)
+            }
+            .subscribe(onNext: { [unowned self] (event) in
+                do {
+                    try self.managedObjectContext.rx.delete(event)
+                } catch {
+                    print(error)
+                }
+            })
+            .addDisposableTo(disposeBag)
+        
+        animatedDataSource.canEditRowAtIndexPath = { _ in
+            return true
+        }
+        animatedDataSource.canMoveRowAtIndexPath = { _ in
+            return true
+        }
+    }
+}
---
+++ Carthage/Checkouts/RxCoreData/RxCoreData.podspec
@@ -0,0 +1,32 @@
+Pod::Spec.new do |s|
+    s.name                  = "RxCoreData"
+    s.version               = "0.3.1"
+    s.summary               = "RxSwift extensions for Core Data"
+    s.description           = <<-DESC
+    Provides types and extensions for working with Core Data. For example, you can create and hook up a Core Data request to a table view with just a few lines of code:
+    ```let fetchRequest = NSFetchRequest(entityName: "User")
+
+    fetchRequest.predicate = NSPredicate(query: "username CONTAINS[cd] %@", searchTerm)
+
+    fetchRequest.sortDescriptors = [NSSortDescriptor(key: "username", ascending: true)]
+
+    managedObjectContext.rx_entities(fetchRequest)
+    .bindTo(tableView.rx_itemsWithDataSource(animatedDataSource))
+    .addDisposableTo(disposeBag)```
+    DESC
+
+    s.homepage              = "https://github.com/RxSwiftCommunity/RxCoreData"
+    s.license               = { :type => "MIT", :file => "LICENSE.md" }
+    s.author                = { "Scott Gardner" => "scott.gardner@mac.com" }
+    s.source                = { :git => "https://github.com/RxSwiftCommunity/RxCoreData.git", :tag => s.version.to_s }
+    s.social_media_url      = "https://twitter.com/scotteg"
+
+    s.ios.deployment_target = '8.0'
+
+    s.source_files = 'Sources/**/*'
+
+    s.frameworks            = 'UIKit', 'CoreData'
+
+    s.dependency 'RxSwift', '~> 3.0'
+    s.dependency 'RxCocoa', '~> 3.0'
+end
